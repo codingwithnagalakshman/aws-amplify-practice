dbcbf72167148bd19f94de972ca481af2307ef9e
Nagalakshman Chirukuri <98049526+codingwithnagalakshman@users.noreply.github.com>
2026-02-20 15:32:10 +0000

    login with phone number

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -6,12 +6,6 @@ import { defineAuth } from "@aws-amplify/backend";
  */
 export const auth = defineAuth({
   loginWith: {
-    email: true,
-  },
-  userAttributes: {
-    preferredUsername: {
-      mutable: true,
-      required: true,
-    },
+    phone: true,
   },
 });
